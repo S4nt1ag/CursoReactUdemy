8e06a217e6b3d24b5172110771aa7f632f4659ee
S4nt1ag <gabrielsantiagoneves2@gmail.com>
2023-08-02 19:40:20 -0300

    aula 14 componentes de classe com estado

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -3,13 +3,33 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
+    state = {
+      name: 'Santiago',
+      counter: 0
+    };
+  
+
+  handlePClick = () => {
+    this.setState({name: 'Gabriel'})
+  }
+
+  handleAClick = () => {
+    const {counter } = this.state;
+    const nextCounter = counter + 1
+    this.setState({counter: nextCounter})
+  }
+
   render() {
+    const { name, counter }= this.state;
     return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
+        <p onClick={this.handlePClick}>
+          {name} {counter}
+        </p>
+        <p onClick={this.handleAClick}>
+          +
         </p>
       </header>
     </div>
