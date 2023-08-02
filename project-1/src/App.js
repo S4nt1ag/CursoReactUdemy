e34517012c25156e29a730ae7f81d857250e5f32
S4nt1ag <gabrielsantiagoneves2@gmail.com>
2023-08-02 19:18:17 -0300

    aula 13 componentes funcionais e de classe

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -12,6 +14,20 @@ function App() {
       </header>
     </div>
   );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello World
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
